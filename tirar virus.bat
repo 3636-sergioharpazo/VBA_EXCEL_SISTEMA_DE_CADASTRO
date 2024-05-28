acf1035c70b8affb1bcc7686a365c9346b280617
Antonio Oliveira - DEV <63135415+3636-sergioharpazo@users.noreply.github.com>
2024-05-28 09:12:11 -0300

    Add files via upload

---
+++ tirar virus.bat
@@ -0,0 +1,14 @@
+@echo off
+cls
+@echo.
+attrib -h -r -s /s /d H:\*.*
+cls
+@echo.
+@echo    ***********************************************
+@echo    ***                                         ***
+@echo    ***  O PENDRIVE FOI RECUPERADO COM SUCESSO !***
+@echo    ***                                         ***
+@echo    ***********************************************
+@echo.
+pause
+
